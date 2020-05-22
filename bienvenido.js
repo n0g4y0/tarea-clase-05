647a664aee57ce3f2734025549835f751b9e0d21
n0g4y0 <n0g4y0@gmail.com>
2020-05-21 22:55:37 -0400

    el usuario saluda
    una vez que el usuario ingresa los datos, se muestra en un campo de texto y en el titulo de la pagina

---
+++ bienvenido.js
@@ -0,0 +1,16 @@
+document.querySelector('#procesar-informacion').onclick = function(){
+    
+    const primerNombre = document.querySelector("#primer-nombre").value;
+    const segundoNombre = document.querySelector("#segundo-nombre").value;
+    const apellidos = document.querySelector("#apellidos").value;
+    const edadUsuario = Number(document.querySelector("#edad").value);
+
+    document.querySelector('#mostrar-informacion').value = primerNombre + " " + segundoNombre + " " + apellidos + " " + edadUsuario;
+    document.querySelector("#titulo").innerHTML = "Bienvenido, " + primerNombre;
+
+    return false;
+}
+
+function calcularSalarioMensual(salarioAnual){
+    return salarioAnual / 12;
+}
